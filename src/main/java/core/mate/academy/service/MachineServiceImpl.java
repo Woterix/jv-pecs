07d35eb46e06b212e0813505251ff58049d54c31
Woterix <dimkabibimka333@gmail.com>
2025-04-21 12:21:59 +0100

    homework done

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,46 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<Machine> machines = new ArrayList<>();
+        if (type.equals(Bulldozer.class)) {
+            BulldozerProducer bulldozerProducer = new BulldozerProducer();
+            machines.addAll(bulldozerProducer.get());
+        }
+        if (type.equals(Excavator.class)) {
+            ExcavatorProducer excavatorProducer = new ExcavatorProducer();
+            machines.addAll(excavatorProducer.get());
+        }
+        if (type.equals(Truck.class)) {
+            TruckProducer truckProducer = new TruckProducer();
+            machines.addAll(truckProducer.get());
+        }
+        return machines;
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, T value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (int i = 0; i < machines.size(); i++) {
+            ((Machine) machines.get(i)).doWork();
+        }
+    }
 }
---
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -0,0 +1,16 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TruckProducer implements MachineProducer<Truck> {
+    @Override
+    public List<Truck> get() {
+        ArrayList<Truck> trucks = new ArrayList<>();
+        trucks.add(new Truck("c12"));
+        trucks.add(new Truck("ca8"));
+        trucks.add(new Truck("545f4s2"));
+        return trucks;
+    }
+}
